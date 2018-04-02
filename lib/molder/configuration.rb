9889c236435fada9f1464b27be5d71714c09bd6f
Konstantin Gredeskoul <kigster@gmail.com>
2018-04-01 22:03:12 -0700

    Fix default config - version 0.1.1

--- lib/molder/configuration.rb
+++ lib/molder/configuration.rb
@@ -4,14 +4,23 @@ require 'hashie/extensions/parsers/yaml_erb_parser'
 
 module Molder
   class Configuration < Hashie::Mash
-    DEFAULT_CONFIG = 'conf/molder.yml'.freeze
+    DEFAULT_CONFIG = 'config/molder.yml'.freeze
     class << self
       def default_config
         DEFAULT_CONFIG
       end
 
       def default
-        load(default_config) if File.exist?(default_config)
+        if File.exist?(default_config)
+          load(default_config)
+        else
+          raise ::Molder::ConfigNotFound, "Default file #{default_config} was not found"
+        end
+
+      end
+
+      def load(file)
+        self.new(YAML.load(File.read(file)))
       end
     end
   end
--- lib/molder/errors.rb
+++ lib/molder/errors.rb
@@ -2,4 +2,5 @@ module Molder
     class MolderError < StandardError; end
     class InvalidCommandError < MolderError; end
     class InvalidTemplateName < MolderError; end
+    class ConfigNotFound < MolderError; end
 end
